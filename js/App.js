947d7477f3c72b8df75f0b9e457e5e6543ed1692
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-04-13 15:25:49 +0900

    Painting Event Create

--- js/App.js
+++ js/App.js
@@ -2,14 +2,22 @@ const canvas = document.getElementById("Canvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("Color");
 const range = document.getElementById("Range");
+const mode = document.getElementById("ModeBtn");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = "700";
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5;
 
+ctx.fillRect(50, 20, 100, 49);
+
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -41,7 +49,7 @@ function onMouseDown(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
-    console.log(color)
+    ctx.fillStyle = color
 }
 
 function handleRangeChange(event) {
@@ -49,11 +57,29 @@ function handleRangeChange(event) {
     ctx.lineWidth = size
 }
 
+function handleModeClick() {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+        ctx.fillStyle = ctx.strokeStyle
+    }
+}
+
+function handleCanvasClick(event) {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+    }
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
+    canvas.addEventListener("click", handleCanvasClick)
 }
 
 Array.from(colors).forEach(color =>
@@ -63,4 +89,8 @@ Array.from(colors).forEach(color =>
 
 if (range) {
     range.addEventListener("input", handleRangeChange)
+}
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick)
 }
