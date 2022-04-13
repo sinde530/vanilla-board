666e2c1264cc9ffced68dcf7c55c1b6a462418b8
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-04-13 14:49:17 +0900

    brush event create

--- js/App.js
+++ js/App.js
@@ -1,36 +1,45 @@
 const canvas = document.getElementById("Canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function stopPainting() {
-    painting = false
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
 }
 
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
     // console.log(x, y)
 }
 
 function onMouseDown(event) {
     // console.log(event)
-    painting = true;
+    startPainting()
 }
-
-function onMouseUp(event) {
-    // console.log(event)
-    stopPainting()
-}
-
-// function onMouseLeave(event) {
-//     stopPainting()
-// }
 
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
 }
